13a796ef172897a00c590388ff2a442203dbfeda
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-03-05 15:24:29 -0500

    added Visible concern  to articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
+    include Visible
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 6 }
 
     has_many :comments
+
 end
